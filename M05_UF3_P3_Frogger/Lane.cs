ca28f68c269458dda93e7de78fa7d1b161df7d0c
Rubén <147936044+rubb-dev@users.noreply.github.com>
2023-10-18 15:53:40 +0200

    all

---
+++ M05_UF3_P3_Frogger/Lane.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M05_UF3_P3_Frogger
+{
+    public class Lane
+    {
+        public readonly int posY;
+        public readonly int speedElements;
+        public readonly bool speedPlayer;
+        public readonly ConsoleColor background;
+        public readonly bool damageElements;
+        public readonly bool damageBackground;
+        public List<DynamicElement> elements { get; protected set; } = new List<DynamicElement>();
+
+        //Construcctor de las lineas
+        public Lane(int posY, bool speedPlayer, ConsoleColor background, bool damageElements, bool damageBackground, float elementsPercent, char elementsChar, List<ConsoleColor> colorsElements)
+        {
+            this.posY = posY;//Posicion de la linea
+            this.speedElements = Utils.rnd.Next(-10, 10) < 0 ? 1 : -1;
+            this.speedPlayer = speedPlayer;
+            this.background = background;
+            this.damageElements = damageElements;
+            this.damageBackground = damageBackground;
+
+            for (int i = 0; i < Utils.MAP_WIDTH; i++)
+            {
+                if(Utils.rnd.NextDouble() < elementsPercent)
+                {
+                    this.elements.Add(
+                        new DynamicElement(
+                            new Vector2Int(speedElements, 0), 
+                            new Vector2Int(i, posY),
+                            elementsChar, 
+                            colorsElements[Utils.rnd.Next(colorsElements.Count)]
+                            ));
+                }
+            }
+            this.elements.TrimExcess();
+        }
+
+        //Funcion dibuja las lineas
+        public void Draw()
+        {
+            Console.SetCursorPosition(0, posY);
+            Console.BackgroundColor = background;
+            for (int i = 0; i < Utils.MAP_WIDTH; i++)
+            {
+                DynamicElement element = ElementAtPosition(new Vector2Int(i, posY));
+                if (element == null)
+                {
+                    Console.Write(' ');
+                }
+                else
+                {
+                    Console.ForegroundColor = element.foreground;
+                    Console.Write(element.character);
+                }
+            }
+        }
+        //Actualiza el dibujado
+        public void Update()
+        {
+            foreach (DynamicElement element in elements)
+            {
+                element.Update();
+            }
+        }
+
+        public DynamicElement ElementAtPosition(Vector2Int position)
+        {
+            foreach (DynamicElement element in elements)
+            {
+                if(element.pos == position)
+                    return element;
+            }
+            return null;
+        }
+    }
+}
+
+//Este objeto nos dibuja todo el mapa introduciendo las distintas lineas que queremos dibujar en una lista
---
+++ M05_UF3_P3_Frogger/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace M05_UF3_P3_Frogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Utils.MAP_HEIGHT = 14;//Ajustael largo del juego
+            // Ajustar el tamaño de la consola
+            Console.SetWindowSize((Utils.MAP_WIDTH) , Utils.MAP_HEIGHT); 
+            
+            List<Lane> lanes = new List<Lane>();
+            lanes.Add(new Lane(0, false, ConsoleColor.DarkGreen, false, false, 0f, ' ', new List<ConsoleColor>()));
+            lanes.Add(new Lane(1, true, ConsoleColor.DarkBlue, false, true, 0.45f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(2, true, ConsoleColor.DarkBlue, false, true, 0.65f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(3, true, ConsoleColor.DarkBlue, false, true, 0.45f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(4, true, ConsoleColor.DarkBlue, false, true, 0.55f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(5, true, ConsoleColor.DarkBlue, false, true, 0.65f, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lanes.Add(new Lane(6, false, ConsoleColor.DarkGreen, false, false, 0f, ' ', new List<ConsoleColor>()));
+            lanes.Add(new Lane(7, false, ConsoleColor.Black, true, false, 0.12f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lanes.Add(new Lane(8, false, ConsoleColor.Black, true, false, 0.15f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lanes.Add(new Lane(9, false, ConsoleColor.Black, true, false, 0.1f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lanes.Add(new Lane(10, false, ConsoleColor.Black, true, false, 0.17f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lanes.Add(new Lane(11, false, ConsoleColor.Black, true, false, 0.1f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lanes.Add(new Lane(12, false, ConsoleColor.Black, true, false, 0.15f, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lanes.Add(new Lane(13, false, ConsoleColor.DarkGreen, false, false, 0f, ' ', new List<ConsoleColor>()));
+            
+            Player player = new Player();
+
+            Utils.GAME_STATE gameState = Utils.GAME_STATE.RUNNING;//Establece el estado del juego corriendo
+            while (gameState == Utils.GAME_STATE.RUNNING)//Comprueba si el estado es corriendo y si no es sale del bucle cuando gana o pierde
+            {
+                TimeManager.NextFrame();//Actualiza la pantalla
+
+                for (int i = 0; i < lanes.Count; i++) //Va dibujando y actualizando las lineas cada frame
+                {
+                    lanes[i].Draw();
+                    lanes[i].Update(); 
+                }
+
+
+                player.Draw(lanes);//Dibuja al jugador
+                Vector2Int inputDir = Utils.Input();//Recoge los inputs
+                gameState = player.Update(inputDir, lanes);//Actualiza el estado del juego
+            }
+
+            Console.Clear();//Limpia la consola
+            Console.SetCursorPosition(0, Utils.MAP_HEIGHT);//Coloca el cursor en las cordenadas q le pasamos
+            if (gameState == Utils.GAME_STATE.WIN)//Si el estado de juego cambia ha WIN muestra el mensaje ¡Has ganado!
+            {
+                Console.WriteLine("¡Has ganado!");
+            }
+            else//Si el estado de juego cambia ha LOSE muestra el mensaje ¡Has perdido!
+            {
+                Console.WriteLine("¡Has perdido!");
+            }
+
+        }
+    }
+}
